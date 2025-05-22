abe5131e4dc5572c31b00a15e8e277c101647e98
337547038 <337547038@qq.com>
2025-05-22 14:06:50 +0800

    init

---
+++ src/main/java/com/flow/ak/AkFlowApplication.java
@@ -0,0 +1,13 @@
+package com.flow.ak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flow/ak/AkFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flow.ak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
